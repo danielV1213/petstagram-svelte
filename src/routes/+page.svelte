2d925b6dc0949e75bae9fd0cfa5f97d3e2f1755b
Daniel Vélez <d.velez@globant.com>
2024-12-14 11:21:00 -0500

    [UPDATE] Build Tool changed to Vite and added template structure for the project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Timeline from "../components/Timeline.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+</script>
+
+<Header />
+<Main>
+  <Timeline />
+  <Sidebar />
+</Main>
+
+<style>
+  :global(body) {
+    background-color: #676778;
+    color: white;
+  }
+</style>
