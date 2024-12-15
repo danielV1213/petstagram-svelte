09ff23a926e5777c38947df19a8a499bb9522ca1
Daniel Andrés Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-12-15 14:58:11 -0500

    [ADD] Header and Sidebar structure

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,1 +1,19 @@
-<div class="header">Header</div>
+<div class="header">
+  <div class="header-container">
+    <div class="header-content">
+      <div class="header-logo">
+        <h1>Pugstagram</h1>
+      </div>
+      <div class="header-nav">
+        <ul>
+          <li>
+            <i class="fa-solid fa-heart"></i>
+          </li>
+          <li>
+            <i class="fa-solid fa-user-alt"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,1 +1,13 @@
-<div class="sidebar">Sidebar</div>
+<script>
+  import Profile from "./Profile.svelte";
+  import Stories from "./Stories.svelte";
+  import Footer from "./Footer.svelte";
+</script>
+
+<div class="sidebar">
+  <div class="sidebar-container">
+    <Profile />
+    <Stories />
+    <Footer />
+  </div>
+</div>
